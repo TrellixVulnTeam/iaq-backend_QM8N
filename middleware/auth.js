f6e7f38611cbab34396793922df3d76c8891bcb2
Vedant Naik <vedantnaik@Vedants-MacBook-Air.local>
2022-07-12 16:44:08 +0100

    changed auth method

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,16 +2,10 @@ const user = "admin";
 const pwd = "iaq-dashboard-2022";
 
 module.exports = async function (req, res, next) {
-  const auth = { login: user, password: pwd };
-
-  const b64auth = (req.headers.authorization || "").split(" ")[1] || "";
-  const [login, password] = Buffer.from(b64auth, "base64")
-    .toString()
-    .split(":");
-
-  if (login && password && login === auth.login && password === auth.password) {
-    return next();
+  const apiKey = req.get("API-Key");
+  if (!apiKey || apiKey !== "simple-key") {
+    res.status(401).json({ error: "unauthorised" });
+  } else {
+    next();
   }
-
-  res.status(401).json({ message: "Authentication required." });
 };
